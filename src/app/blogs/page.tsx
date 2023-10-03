e55dfd82b607ab74b2277891b16c915e969dd10a
fibbbonachos <arish2k3@gmail.com>
2023-10-03 14:56:35 +0530

    add: navbar and footer global components and also add blogs route

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../_globalComponents/Navbar";
+import Footer from "../_globalComponents/Footer";
+
+const page = () => {
+  return (
+    <div>
+      <Navbar />
+      blogs page
+      <Footer />
+    </div>
+  );
+};
+
+export default page;
