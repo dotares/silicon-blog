3f8e185d063a5ff91d93878b68d64883006c7735
fibbbonachos <arish2k3@gmail.com>
2023-10-05 16:31:48 +0530

    update: navbar with social links

--- src/app/_globalComponents/Navbar.tsx
+++ src/app/_globalComponents/Navbar.tsx
@@ -3,7 +3,24 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="https://twitter.com/fibbbonachos">Twitter</a>
+                </li>
+                <li>
+                    <a href="https://hashnode.com/@Fibz">Hashnode</a>
+                </li>
+                <li>
+                    <a href="https://github.com/fibbbonachos">GitHub</a>
+                </li>
+                <li>
+                    <a href="https://fibbonachos.vercel.app/">Portfolio</a>
+                </li>
+            </ul>
+        </div>
+    );
 };
 
 export default Navbar;
