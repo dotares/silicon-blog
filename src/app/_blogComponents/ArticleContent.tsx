35145be8a4f0ee5eebce95f1a657404fb4e5df95
fibbbonachos <arish2k3@gmail.com>
2023-10-05 17:05:08 +0530

    add: more queries and information for each article information

--- src/app/_blogComponents/ArticleContent.tsx
+++ src/app/_blogComponents/ArticleContent.tsx
@@ -9,12 +9,30 @@ export interface ArticleContentProps {
 
 const ArticleContent: React.FC<ArticleContentProps> = async ({ slug }) => {
     const data = await articleContentFetcher(slug);
-    const htmlData = data.publication.post.content.html;
+
+    const articleTitle = data.publication.post.title;
+    const articleCoverImage = data.publication.post.coverImage.url;
+    const articleContent = data.publication.post.content.html;
+    const articleUrl = data.publication.post.url;
+    const articleReadTime = data.publication.post.readTimeInMinutes;
+    const articlePublishedAt = data.publication.post.publishedAt;
+    const articleReactionCount = data.publication.post.reactionCount;
+    const articleResponseCount = data.publication.post.responseCount;
+    const articleViews = data.publication.post.views;
 
     return (
         <div>
-            <h1>{data.publication.post.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: htmlData }}></div>
+            <img src={articleCoverImage} alt="Article's cover image" />
+            <h1>{articleTitle}</h1>
+            <div>
+                <p>Reads {articleViews}</p>
+                <p>Likes {articleReactionCount}</p>
+                <p>Comments {articleResponseCount}</p>
+                <p>readTime {articleReadTime}</p>
+                <p>published {articlePublishedAt}</p>
+            </div>
+            <a href={articleUrl}>Read on Hashnode</a>
+            <div dangerouslySetInnerHTML={{ __html: articleContent }}></div>
         </div>
     );
 };
