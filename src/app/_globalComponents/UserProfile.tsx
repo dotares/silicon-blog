3c68f34344af060563a015332ce22f0832b81bdd
fibbbonachos <arish2k3@gmail.com>
2023-10-05 18:01:30 +0530

    update: hero details section

--- src/app/_globalComponents/UserProfile.tsx
+++ src/app/_globalComponents/UserProfile.tsx
@@ -9,21 +9,20 @@ const UserProfile = () => {
     if (!data) return <p>Loading ...</p>;
     if (error) return <p>Error</p>;
     return (
-        <div>
-            <div>
-                <h1>The Silicon Blog</h1>
-                <div>
-                    by
-                    <div>
-                        <img
-                            src={data.user.profilePicture}
-                            alt="Fibz's profile picture"
-                        />{" "}
-                        <a href="https://hashnode.com/@Fibz">Fibz</a>
-                    </div>
+        <div className="space-y-4 p-4">
+            <div className="space-y-4">
+                <h1 className="text-4xl text-center">The Silicon Blog</h1>
+                <div className="text-lg space-x-2 flex items-center justify-center">
+                    <p className="text-xl text-center">by</p>
+                    <img
+                        className="h-12 w-12 rounded-full"
+                        src={data.user.profilePicture}
+                        alt="Fibz's profile picture"
+                    />{" "}
+                    <a href="https://hashnode.com/@Fibz">Fibz</a>
                 </div>
             </div>
-            <div>
+            <div className="flex flex-col text-center space-y-2">
                 <p>{data.user.bioV2.markdown}</p>
                 <p>{data.user.followersCount} Followers</p>
             </div>
