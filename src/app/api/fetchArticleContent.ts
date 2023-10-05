160e5093b35ae2439b8e53cd1d742622e0e08243
fibbbonachos <arish2k3@gmail.com>
2023-10-05 12:34:25 +0530

    remove: console logs in dev

--- src/app/api/fetchArticleContent.ts
+++ src/app/api/fetchArticleContent.ts
@@ -16,7 +16,6 @@ query ($slug: String!){
           `;
 
 export const articleContentFetcher: React.FC<string> = async (slug) => {
-    console.log(`This is from articleContentFetcher: ${slug}`);
     const variables = { slug: slug };
     const response = await fetch("https://gql.hashnode.com", {
         body: JSON.stringify({ query, variables }),
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ArticleContent from "../../_blogComponents/ArticleContent";
 
 const page: React.FC<any> = ({ params }) => {
-    console.log(params.slug);
-
     return (
         <div>
             <ArticleContent slug={params.slug} />
