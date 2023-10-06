0bc84a76f418426a0f03845afdce363f6e304841
fibbbonachos <arish2k3@gmail.com>
2023-10-06 18:40:11 +0530

    remove: articles get animation framer

--- src/app/_blogComponents/Articles.tsx
+++ src/app/_blogComponents/Articles.tsx
@@ -30,14 +30,7 @@ const Articles = () => {
             {data.publication.posts.edges.map(
                 (result: ResultsObject, index: number) => {
                     return (
-                        <motion.div
-                            initial={{ opacity: 0, scale: 0.5 }}
-                            animate={{ opacity: 1, scale: 1 }}
-                            transition={{
-                                duration: 0.8,
-                                delay: 0.5,
-                                ease: [0, 0.71, 0.2, 1.01],
-                            }}
+                        <div
                             key={index}
                             className="flex justify-center drop-shadow-lg"
                         >
@@ -50,7 +43,7 @@ const Articles = () => {
                                     />
                                 </Link>
                             </div>
-                        </motion.div>
+                        </div>
                     );
                 }
             )}
