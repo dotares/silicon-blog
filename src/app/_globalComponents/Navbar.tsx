0cdb53d335e0fc3fcb1dd12831e5f9f888c12c8f
fibbbonachos <arish2k3@gmail.com>
2023-10-06 18:28:17 +0530

    add: navbar animations

--- src/app/_globalComponents/Navbar.tsx
+++ src/app/_globalComponents/Navbar.tsx
@@ -2,20 +2,27 @@
 
 import React from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
     return (
         <div>
             <ul className="flex justify-center sm:space-x-12 items-center space-x-5 p-10">
-                <li>
+                <motion.li
+                    whileHover={{ scale: 1.1 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                >
                     <Link
                         href="/"
                         className="font-mono text-xl font-bold text-foregroundz2"
                     >
                         Fibbonachos.
                     </Link>
-                </li>
-                <li>
+                </motion.li>
+                <motion.li
+                    whileHover={{ scale: 1.2 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                >
                     <a href="https://twitter.com/fibbbonachos">
                         <svg
                             className="fill-buttons"
@@ -27,8 +34,11 @@ const Navbar = () => {
                             <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
                         </svg>
                     </a>
-                </li>
-                <li>
+                </motion.li>
+                <motion.li
+                    whileHover={{ scale: 1.2 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                >
                     <a href="https://hashnode.com/@Fibz">
                         <svg
                             width="24"
@@ -51,8 +61,11 @@ const Navbar = () => {
                             />
                         </svg>
                     </a>
-                </li>
-                <li>
+                </motion.li>
+                <motion.li
+                    whileHover={{ scale: 1.2 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                >
                     <a href="https://github.com/fibbbonachos">
                         <svg
                             width="24"
@@ -68,8 +81,11 @@ const Navbar = () => {
                             />
                         </svg>
                     </a>
-                </li>
-                <li>
+                </motion.li>
+                <motion.li
+                    whileHover={{ scale: 1.2 }}
+                    transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                >
                     <a href="https://fibbonachos.vercel.app/">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -81,7 +97,7 @@ const Navbar = () => {
                             <path d="M176 56V96H336V56c0-4.4-3.6-8-8-8H184c-4.4 0-8 3.6-8 8zM128 96V56c0-30.9 25.1-56 56-56H328c30.9 0 56 25.1 56 56V96v32V480H128V128 96zM64 96H96V480H64c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64zM448 480H416V96h32c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64z" />
                         </svg>
                     </a>
-                </li>
+                </motion.li>
             </ul>
         </div>
     );
